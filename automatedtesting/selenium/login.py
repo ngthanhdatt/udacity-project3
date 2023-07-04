fffc4c92b6b418054d4154e8370137f834c0739b
ngthanhdatt <thanhdat12a2ntt99@gmail.com>
2023-07-04 16:58:29 +0700

    update

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,79 +2,75 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
-import logging
 import datetime
+
+login_url = 'https://www.saucedemo.com/'
+inventory_url = 'https://www.saucedemo.com/inventory.html'
+cart_url = 'https://www.saucedemo.com/cart.html'
 
 def log():
     time = datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-    return ("LOG" + time)
+    return ("LOG " + time)
 
-
-URL_LOGIN = 'https://www.saucedemo.com/'
-URL_INVENTORY = 'https://www.saucedemo.com/inventory.html'
-URL_CART = 'https://www.saucedemo.com/cart.html'
+def startup():
+    print(f'{log()} Starting the browser...')
+    options = ChromeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    print (f'{log()} Browser started successfully. Navigating to the demo page to login.')
+    return driver
 
 # Start the browser and login with standard_user
 def login (driver, user, password):
-    print (f'{log()} Starting the browser...')
-    print (f'{log()} Browser started successfully. Navigating to the demo page to login.')
-    driver.get(URL_LOGIN)
-    username = driver.find_element(By.ID, 'user-name')
-    username.send_keys(user)
-    print(f'{log()} Username: {user} ')
+    driver.get(login_url)
+    driver.find_element(By.ID, 'user-name').send_keys(user)
+    driver.find_element(By.ID, 'password').send_keys(password)
+    driver.find_element(By.ID,'login-button').click()
+    assert inventory_url in driver.current_url, "Error, login failed!"
+    print(f'{log()} User {user} logged in successfully')
 
-    password = driver.find_element(By.ID, 'password')
-    password.send_keys(password)
-    print(f'{log()} Password ')
+def add_products(driver):
+    print(f'{log()} Starting to add products to the cart')
+    
+    cart_items = []
+    products = driver.find_elements(By.CLASS_NAME, 'inventory_item')
 
-    login = driver.find_element(By.ID,'login-button')
-    print(f'{log()} Username and password submitted')
-    login.click()
+    for product in products:
+        product_item = product.find_element(By.CLASS_NAME, 'inventory_item_name').text
+        cart_items.append(product_item)
+        product.find_element(By.CLASS_NAME, 'btn_inventory').click()
+        print(f'{log()} {product_item} added to the cart')
 
-    assert URL_INVENTORY in driver.current_url
-    print (f'{log()} Test Login Success')
+    cart = driver.find_element(By.CLASS_NAME, 'shopping_cart_badge')
+    assert int(cart.text) == len(products), 'ERROR: The cart count does not match the number of products added'
 
-def add_items(driver):
-    print (f'{log()} Add all items to the cart')
-    shopping_cart = []
-    list_items = driver.find_elements(By.CLASS_NAME,"inventory_item")
-    for i in list_items:
-        item_name = i.find_element(By.CLASS_NAME,'inventory_item_name').text
-        shopping_cart.append(item_name)
-        i.find_element(By.CLASS_NAME,'btn_inventory').click()
-        print(f'{log()} {item_name} added to the cart')
-    cart_item = driver.find_element(By.CLASS_NAME,'shopping_cart_badge')
-    assert int(cart_item.text) == len(list_items)
+    for item in driver.find_elements(By.CLASS_NAME, 'inventory_item_name'):
+        assert item.text in cart_items, 'ERROR: All products are not added to the cart'
 
+    print(f'{log()} All products added to the cart successfully')
+
+def delete_products(driver):
+    print(f'{log()} Navigating to cart page')
     driver.find_element(By.CLASS_NAME,'shopping_cart_link').click()
-    assert URL_CART in driver.current_url
+    assert cart_url in driver.current_url
 
-    for i in driver.find_elements(By.CLASS_NAME,'inventory_item_name'):
-        assert i.text in shopping_cart
-    print(f'{log()} Finished testing adding items to the cart')
+    cart_items = driver.find_elements(By.CLASS_NAME,'cart_item')
+    cart_items_count = len(cart_items)
+    print(f'{log()} Number of items in the cart {cart_items_count}')
 
+    for product in cart_items:
+        product_item = product.find_element(By.CLASS_NAME,'inventory_item_name').text
+        product.find_element(By.CLASS_NAME,'cart_button').click()
+        print(f'{log()} {product_item} removed from the cart')
 
-def remove_items(driver):
-    print (f'{log()} Remove items from the cart')
-    driver.find_element(By.CLASS_NAME,'shopping_cart_link').click()
-    assert URL_CART in driver.current_url
-    for i in driver.find_elements(By.CLASS_NAME,'cart_item'):
-        item_name = i.find_element(By.CLASS_NAME,'inventory_item_name').text
-        i.find_element(By.CLASS_NAME,'cart_button').click()
-        print(f'{log()} {item_name} removed from the cart')
-    cart_items = len(driver.find_elements(By.CLASS_NAME,'cart_item'))
-    assert cart_items == 0
-    print(f'{log()} Finshed testing removing items from the cart')
+    cart_items_count = len(driver.find_elements(By.CLASS_NAME,'cart_item'))
+    assert cart_items_count == 0,'ERROR: Not all products were removed from the cart'
 
-def tests():
-    options = ChromeOptions()
-    options.add_argument("--no-sandbox")
-    options.add_argument("--headless") 
-    driver = webdriver.Chrome(options=options)
-    login(driver, "standard_user", "secret_sauce")
-    add_items(driver)
-    remove_items(driver)
-    print(f'{log()} Tests Completed')
+    print(f'{log()} All products removed from the cart successfully')
 
 if __name__ == "__main__":
-    tests()
+    driver_active = startup()
+    login(driver_active, 'standard_user', 'secret_sauce')
+    add_products(driver_active)
+    delete_products(driver_active)
